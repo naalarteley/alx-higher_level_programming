462161f266de2b6832c0eb804ec6c03cb4627f82
Hazel <hazellarkai26@gmail.com>
2024-03-13 20:33:31 -0700

    Am I doing thissss riiighttt

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stddef.h> // Include this for size_t and NULL
+#include <stdlib.h> // Include this for malloc
 
 /**
  * insert_node - Inserts a number into a sorted singly-linked list.
@@ -8,25 +10,25 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+    listint_t *node = *head, *new;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+    new->n = number;
 
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
+    if (node == NULL || node->n >= number)
+    {
+        new->next = node;
+        *head = new;
+        return (new);
+    }
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+    while (node && node->next && node->next->n < number)
+        node = node->next;
 
-	new->next = node->next;
-	node->next = new;
+    new->next = node->next;
+    node->next = new;
 
-	return (new);
+    return (new);
 }
